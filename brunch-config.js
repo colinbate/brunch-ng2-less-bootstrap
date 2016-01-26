acdc31333853c127bc70161920f31438d0c77dbc
Colin Bate <colin@colinbate.com>
2016-01-26 16:11:16 -0400

    Change config to JS, add packages.

---
+++ brunch-config.js
@@ -0,0 +1,23 @@
+exports.config = {
+  // See http://brunch.io/#documentation for docs.
+  npm: {
+    enabled: true,
+    globals: {
+      Reflect: 'es7-reflect-metadata/dist/browser'
+    }
+  },
+  files: {
+    javascripts: {
+      joinTo: {
+        'vendor.js': /^(app\/vendor)|(node_modules)/,
+        'main.js': /^app\/(?!vendor)/
+      }
+    },
+    stylesheets: {
+      joinTo: 'app.css'
+    },
+    templates: {
+      joinTo: 'main.js'
+    }
+  }
+};
