077ead7a4c39013a75dcd7921e7035038a2e25e1
Colin Bate <colin@colinbate.com>
2016-01-27 17:14:52 -0400

    Update nav to use directive.

--- app/components/nav/index.ts
+++ app/components/nav/index.ts
@@ -1,11 +1,12 @@
 import {Component} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
-//import {RouterActive} from '../../directives/router-active';
+import {RouterActive} from './../../directives/router-active';
 
 @Component({
   selector: 'app-nav',
   directives: [
-    ...ROUTER_DIRECTIVES
+    ...ROUTER_DIRECTIVES,
+    RouterActive
   ],
   styles: [ require('./nav.style') ],
   template: require('./nav.tmpl')()
